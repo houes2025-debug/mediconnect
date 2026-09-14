d7d5935599c3d0698308e00d261771e2645ca96a
houes2025-debug <houes.2025@gmail.com>
2026-09-15 01:37:56 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Dashboard />} />
         <Route path="/app" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="/" replace />} />
